0a594ca62184dfe71e73770b46ee0ead9dbec2f2
profflaviomoreni <profflavio.moreni@fiap.com.br>
2022-06-26 12:54:56 -0300

    contador e botoes

--- src/screens/Home/View/HomeStyle.js
+++ src/screens/Home/View/HomeStyle.js
@@ -3,11 +3,22 @@ import {StyleSheet} from 'react-native';
 export default StyleSheet.create({
   container: {
     flexDirection: 'column',
-    flex: 1,
-    justifyContent: 'center',
+    flex: 1,    
+    marginTop: 50,
+    alignContent: "center",
+    alignItems: "center",
   },
   textInfo: {
     fontSize: 50,
     textAlign: 'center',
+  },
+  viewButton: {
+    backgroundColor: '#ff000040',
+    marginTop: 15,
+    width: 200,
+    height: 50,
+    fontSize: 50,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- src/screens/Home/View/HomeView.js
+++ src/screens/Home/View/HomeView.js
@@ -2,24 +2,39 @@ import React from 'react';
 import {
   View,
   Text,
+  TouchableOpacity,
+  TouchableHighlight, 
+  TouchableWithoutFeedback,
 } from 'react-native';
 import styles from './HomeStyle' //Importando o Style
 
-class HomeView extends React.Component {
-
-  constructor() {
-    super();
-  }
-
-  render() {
-    const teste = "Hello World";
-
+const HomeView = (props) => {
+    //juntando o texto Tempo com o count_info vindo do props
+    const teste = "Tempo: " + props.count_info;
+    
+    //Passando o metodo recebido pela props no onPress
     return (
       <View style={styles.container}>
         <Text style={styles.textInfo}> {teste} </Text>
+        <TouchableOpacity underlayColor="white" onPress={props.onButtonPressed}>
+          <View style={styles.viewButton} >
+            <Text>TouchableOpacity</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableHighlight underlayColor="white" onPress={props.onButtonPressed}>
+          <View style={styles.viewButton} >
+            <Text>TouchableHighlight</Text>
+          </View>
+        </TouchableHighlight>
+        <TouchableWithoutFeedback onPress={props.onButtonPressed} >
+          <View style={styles.viewButton}>
+            <Text >TouchableWithoutFeedback</Text>
+          </View>
+        </TouchableWithoutFeedback>
+
       </View>
     )
-  }
-}
 
+
+}
 export default HomeView;
